6af6151d8504e5bef55615acb8b8a1f02f0f0636
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-01-18 17:23:22 +0100

    The write metod on the client

--- client.go
+++ client.go
@@ -24,3 +24,15 @@ func (c *client) read() {
 		c.room.forward <- msg
 	}
 }
+
+func (c *client) write() {
+	defer c.socket.Close()
+
+	for msg := range c.send {
+		err := c.socket.WriteMessage(websocket.TextMessage, msg)
+
+		if err != nil {
+			return
+		}
+	}
+}
