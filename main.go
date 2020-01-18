256fc37545167fc16088f8573d27a65a9886ce61
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-01-18 17:05:08 +0100

    ServeHttp method for the template handler

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
+	"path/filepath"
 	"sync"
 	"text/template"
 )
@@ -11,6 +12,13 @@ type templateHandler struct {
 	once     sync.Once
 	filename string
 	templ    *template.Template
+}
+
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
+	})
+	t.templ.Execute(w, nil)
 }
 
 func main() {
