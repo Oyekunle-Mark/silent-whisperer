b0948c12682addb7f1235127c17eb5adbffccc14
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-01-19 09:47:33 +0100

    New method

--- trace/tracer.go
+++ trace/tracer.go
@@ -1,6 +1,10 @@
 package trace
 
+import "io"
+
 // Tracer is the interface that describes an object capable of // tracing events throughout code.
 type Tracer interface {
 	Trace(...interface{})
 }
+
+func New(w io.Writer) {}
