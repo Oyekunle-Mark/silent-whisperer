9a58b7ba97c3bfe3a950cc97c6c47eed490840da
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-01-19 09:45:27 +0100

    Test the new and trace method

--- trace/tracer_test.go
+++ trace/tracer_test.go
@@ -1,7 +1,21 @@
 package trace
 
-import "testing"
+import (
+	"bytes"
+	"testing"
+)
 
 func TestNew(t *testing.T) {
-	t.Error("No test yet")
+	var buf bytes.Buffer
+	tracer := New(&buf)
+
+	if tracer == nil {
+		t.Error("Return from New should not be nil")
+	} else {
+		tracer.Trace("Hello trace package.")
+
+		if buf.String() != "Hello trace package.\n" {
+			t.Errorf("Trace should not write '%s'.", buf.String())
+		}
+	}
 }
