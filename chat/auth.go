2cc0e0e0fe3f4f687861056fa9afc33e35736062
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-01-28 14:03:52 +0100

    Login handler func

--- chat/auth.go
+++ chat/auth.go
@@ -1,6 +1,11 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+)
 
 type authHandler struct {
 	next http.Handler
@@ -21,4 +26,20 @@ func (a *authHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 // MustAuth protects a handler using authHandler
 func MustAuth(handler http.Handler) http.Handler {
 	return &authHandler{next: handler}
+}
+
+// loginHandler handles the third-party login process.
+// format: /auth/{action}/{provider}
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+	segments := strings.Split(r.URL.Path, "/")
+	action := segments[2]
+	provider := segments[3]
+
+	switch action {
+	case "login":
+		log.Println("TODO handle login for", provider)
+	default:
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, "Auth action %s not supported", action)
+	}
 }
