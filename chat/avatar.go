a6809e52dfc155eaf38ce21199eff596aed653ff
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-01-30 13:21:06 +0100

    Get avatar url handles any file extension

--- chat/avatar.go
+++ chat/avatar.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"errors"
+	"io/ioutil"
+	"path"
 )
 
 // ErrNoAvatarURL is the error that is returned when the
@@ -63,7 +65,21 @@ var UseFileSystemAvatar FileSystemAvatar
 func (FileSystemAvatar) GetAvatarURL(c *client) (string, error) {
 	if userid, ok := c.userData["userid"]; ok {
 		if useridStr, ok := userid.(string); ok {
-			return "/avatars/" + useridStr + ".jpg", nil
+			files, err := ioutil.ReadDir("avatars")
+
+			if err != nil {
+				return "", ErrNoAvatarURL
+			}
+
+			for _, file := range files {
+				if file.IsDir() {
+					continue
+				}
+
+				if match, _ := path.Match(useridStr+"*", file.Name()); match {
+					return "/avatars/" + file.Name(), nil
+				}
+			}
 		}
 	}
 
